a52e4d0a01d11f0e5f792656d9d84b7aa1ce215e
unknown <robinrythm123@gmail.com>
2022-02-17 13:21:17 +0530

    added rate limiter and other validations

--- src/sms_service/serializers.py
+++ src/sms_service/serializers.py
@@ -1,8 +1,8 @@
-from asyncio.windows_events import NULL
-from rest_framework import serializers
-from .utils import validate_input
-from .models import Account, PhoneNumber
 from django.core.cache import cache
+from rest_framework import serializers
+
+from .models import Account, PhoneNumber
+from .utils import validate_input
 
 
 class InboundSMSSerializer(serializers.Serializer):
@@ -16,11 +16,17 @@ class InboundSMSSerializer(serializers.Serializer):
         if data.get("to") not in number_list:
             response["error"] = "to parameter not found"
             raise serializers.ValidationError(response)
-        response["message"] = "inbound sms ok"
-        return response
+        return data
 
     def create(self, validated_data):
-        return validated_data
+        text = validated_data.get("text")
+        if text in ["STOP", "STOP\n", "STOP\r", "STOP\r\n"]:
+            _from = validated_data.get("from")
+            _to = validated_data.get("to")
+            if not cache.get(_from):
+                cache.set(_from, _to, timeout=14400)
+        response = {"message" : "inbound sms ok", "error" : ""}
+        return response
 
 
 class OutboundSMSSerializer(serializers.Serializer):
@@ -29,5 +35,28 @@ class OutboundSMSSerializer(serializers.Serializer):
         validate_input(data, response)
         if response["error"]:
             raise serializers.ValidationError(response)
+        account = Account.objects.get(username=self.context.get("user").username)
+        number_list = PhoneNumber.objects.filter(account=account.id).values_list('number', flat=True)
+        if data.get("from") not in number_list:
+            response["error"] = "from parameter not found"
+            raise serializers.ValidationError(response)
+        return data
+
+    def create(self, validated_data):
+        response = {"message" : "", "error" : ""}
+        _from = validated_data.get("from")
+        _to = validated_data.get("to")
+        if cache.get(_from) == _to:
+            response["error"] = f"sms from {_from} to {_to} blocked by STOP request"
+            return response
+        cache_key = _from + " count"
+        total_requests = cache.get(cache_key)
+        if not total_requests:
+            cache.set(cache_key, 1, timeout=86400)
+        elif total_requests < 5:
+            cache.set(cache_key, total_requests + 1)
+        else:
+            response["error"] = f"limit reached for from {_from}"
+            return response
         response["message"] = "outbound sms ok"
         return response
--- src/sms_service/views.py
+++ src/sms_service/views.py
@@ -11,6 +11,6 @@ class InboundSMSAPIView(views.APIView):
 
 class OutboundSMSAPIView(views.APIView):
     def post(self, request):
-        serializer = OutboundSMSSerializer(data=request.data)
+        serializer = OutboundSMSSerializer(data=request.data, context = {"user" : request.user})
         serializer.is_valid(raise_exception=True)
-        return response.Response(data=serializer.validated_data, status=status.HTTP_200_OK)
+        return response.Response(data=serializer.save(), status=status.HTTP_200_OK)
